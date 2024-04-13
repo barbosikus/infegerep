3ebf8152c2585702c972479060a9d6792285df96
Amd <ulk76@mail.ru>
2024-04-13 19:56:34 +0300

    занятие 11.04

--- 14/12.py
+++ 14/12.py
@@ -1,6 +1,20 @@
 """
 Определите число x, для которого выполняется равенство 103ₓ = 97ₓ₊₂.
 """
-for i in range(10, 30):
-    if int("103", i) == int("97", i+2):
-        print(i)
+n1 = 103
+n2 = 97
+for x in range(2,100):
+    '''for y in range(2, 100):'''
+    y =x+2
+    b = ''
+    c = ''
+    while n1>0:
+        b += str(n1%x)
+        n1 //=x
+    b = b[::-1]
+    while n2>0:
+        c += str(n2%y)
+        n2 //=y
+    c = c[::-1]
+    if b == c:
+        print(x)
