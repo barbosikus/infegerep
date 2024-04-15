d40f39d8ebed85be7aa7c8e073e21d964f668083
Amd <ulk76@mail.ru>
2024-04-15 20:47:51 +0300

    дз к 18.04 14 задание

--- 14/15.py
+++ 14/15.py
@@ -3,6 +3,17 @@
 в системе счисления с основанием 3 заканчивается на 11.
 """
 def tri(x):
+    t = ''
+    while x>0:
+        t+=str(x%3)
+        x//=3
+    t = t[::-1]
+    return t
+
+for x in range(21,30):
+    if tri(x)[-2:] == '11':
+        print(x)
+def tri(x):
     # x=переданное
     t = ''
     while x > 0:
