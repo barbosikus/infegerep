6c7cc057b1ec98d8a58c0ada2cb9218ea5ed3f0b
Andrew Plutto <pluttan@ya.ru>
2024-05-02 20:21:04 +0300

    код 11

--- 12/11.py
+++ 12/11.py
@@ -13,3 +13,17 @@
 при котором значения a и b можно выбрать так, что после выполнения
 алгоритм Чертёжник окажется в точке (1; –3).
 """
+
+from turtle import *
+tracer(0)
+
+for i in range(0, 100):
+    for a in range(-20, 20):
+        for b in range(-20, 20):
+            goto(-7, -1)
+            for n in range(0, i):
+                        goto(xcor() + 15, ycor() + 22)
+                        goto(xcor() + a, ycor() + b)
+            goto(xcor() + 23, ycor() - 32)
+            if xcor() == 1 and ycor() == -3:
+                print(xcor(), ycor(), i,a,b)
